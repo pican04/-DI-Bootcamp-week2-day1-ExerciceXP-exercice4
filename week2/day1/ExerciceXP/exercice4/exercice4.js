0a5b39a211fa4ec7a935f6cf9b69a25d48f1ae27
Nadia <nadiagueu@gmail.com>
2022-12-28 02:05:37 +0000

    week2 day1 exerciceXP4

---
+++ week2/day1/ExerciceXP/exercice4/exercice4.js
@@ -0,0 +1,17 @@
+let c;
+let a = 34;
+let b = 21;
+
+console.log(a+b)
+// Prediction: It will output 55, because 21 and 34 are numbers
+// Actual: 55
+
+a = 2;
+console.log(a+b) //second expression
+// Prediction: It will output 23, because 2 and 21 are numbers
+// Actual: 23
+
+// c value is : undefined
+console.log(3 + 4 + '5');
+// Prediction: this syntax is incorrect but It will output the concatenation of the sum of 7(3+4) and the character 5
+// Actual: 75
